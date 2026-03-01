7ecd1ce84968a84dbe3bc3d4ca904fcdfe06d9a5
Tihomir Georgiev <georgiev.tm@abv.bg>
2026-03-01 13:15:38 +0100

    added IPg VectorRepository as Interface in Repositories folder

---
+++ AiService/Repositories/IPgVectorRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace AiService.Repositories
+{
+    public interface IPgVectorRepository
+    {
+        Task InsertVectorAsync(
+            string productId,
+            string name,
+            string summary,
+            string description,
+            string imageFile,
+            string brandId,
+            string brandName,
+            string typeId,
+            string typeName,
+            DateTimeOffset createdDate,
+            float[] embedding);
+
+        Task<IEnumerable<Product>> SearchByVectorAsync(float[] queryVector, int topK = 5);
+
+        Task<IEnumerable<Product>> SearchByKeywordAsync(string keyword, int topK = 5);
+
+        Task<IEnumerable<Product>> SearchByVectorAsync(string query, float[] queryVector, int topK = 5);
+    }
+}
