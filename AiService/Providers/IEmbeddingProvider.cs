00d2a4c835e33ac4da14c5016ea1889bfc6bc9b6
Tihomir Georgiev <georgiev.tm@abv.bg>
2026-03-01 13:33:06 +0100

    added IEmbeddingProvider as interface in Providers folder

---
+++ AiService/Providers/IEmbeddingProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace AiService.Providers
+{
+    public interface IEmbeddingProvider
+    {
+        Task<float[]> EmbedAsync(string text, CancellationToken cancellationToken);
+
+        Task<float[][]> EmbedBatchAsync(IEnumerable<string> texts, CancellationToken cancellationToken);
+
+        int Dimension { get; } // eg 768, 1536
+    }
+}
