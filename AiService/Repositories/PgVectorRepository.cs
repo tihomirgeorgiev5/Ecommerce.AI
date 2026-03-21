b85295871997af39fa5974b273f628fa963bf60b
Tihomir Georgiev <georgiev.tm@abv.bg>
2026-03-21 21:07:51 +0100

    added PgVectorRepository class

---
+++ AiService/Repositories/PgVectorRepository.cs
@@ -0,0 +1,34 @@
+﻿using AiService.Models;
+using Npgsql;
+
+namespace AiService.Repositories
+{
+    public class PgVectorRepository : IPgVectorRepository
+    {
+        private readonly NpgsqlDataSource _dataSource;
+
+        public PgVectorRepository(NpgsqlDataSource dataSource)
+        {
+            _dataSource = dataSource;
+        }
+        public Task InsertVectorAsync(string productId, string name, string summary, string description, string imageFile, string brandId, string brandName, string typeId, string typeName, DateTimeOffset createdDate, float[] embedding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> SearchByKeywordAsync(string keyword, int topK = 5)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> SearchByVectorAsync(float[] queryVector, int topK = 5)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> SearchByVectorAsync(string query, float[] queryVector, int topK = 5)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
