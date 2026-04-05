efe2384c261dfd1526550c2080ef9b5fcc1c1d10
Tihomir Georgiev <georgiev.tm@abv.bg>
2026-04-05 10:32:26 +0200

    added class ChatRespones as record in Models to can use it in new interface IChatService

---
+++ AiService/Models/ChatResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AiService.Models
+{
+    public record ChatResponse(
+        string Answer,
+        IEnumerable<SourceResult> Sources = null,
+        IEnumerable<Product>? Products = null
+        );
+   
+}
---
+++ AiService/Services/IChatService.cs
@@ -0,0 +1,16 @@
+﻿using AiService.Models;
+
+namespace AiService.Services
+{
+    /// <summary>
+    /// Ask Chatbot a question
+    /// It will first in product db and if not found, fallback to web search + LLM to generate answer
+    /// </summary>
+    public interface IChatService
+    {
+        Task<ChatResponse> AskAsync(
+            string userQuery,
+            CancellationToken cancellationToken = default,
+            IEnumerable<(string Role, string Content)>? chatHistory = null);
+    }
+}
