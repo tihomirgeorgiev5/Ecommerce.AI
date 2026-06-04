0447ccc8cb03f60e8828f0e03315d240c23a15ec
Tihomir Georgiev <georgiev.tm@abv.bg>
2026-06-04 18:17:12 +0200

    added new class ChatCompletionResponse

---
+++ AiService/Models/ChatCompletionResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace AiService.Models
+{
+    public class ChatCompletionResponse
+    {
+        public List<Choice> choices { get; set; } = new();
+
+        public class Choice
+        {
+            public Message message { get; set; } = new();
+        }
+
+        public class Message
+        {
+            public string role { get; set; }
+            public string content { get; set; }
+        }
+    }
+}
